6426e6c2cd1ac8bc5afd1e361113c69530044d21
panagiotis <panagiotis4411@gmail.com>
2023-11-25 15:01:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ergasiamellon/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ergasiamellon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ergasiamellon.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+        setTitle("Counter app");
+
+    }
+}
